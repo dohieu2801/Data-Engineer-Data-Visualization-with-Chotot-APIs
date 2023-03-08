58e23e0ff306ad48bab544ba26577027f3b481a0
Thanhthuy-200997 <92812173+Thanhthuy-200997@users.noreply.github.com>
2023-03-08 15:15:27 +0700

    Update crawler.py

--- src/utils/crawler.py
+++ src/utils/crawler.py
@@ -18,7 +18,7 @@ def crawler():
     href = cfg['href']
     data_crawl = []
     df = pd.DataFrame()
-    for cate in range(len(cate_list)):
+        for cate in range(len(cate_list)):
         offset_var = 0
         limit_var = 100
         # respone_total =requests.get(href +"&cg=" + str(cate_list[cate]) + "&st=s,k")
@@ -28,13 +28,14 @@ def crawler():
         categories = cate_list[cate]
         for i in range(0, 200):
             response_API = requests.get(href + "limit=" + str(offset) + "&o=" + str(limit) + "&cg=" + str(categories) + "&st=s,k")
+            if response_API.status_code==200:
             data = response_API.text
-            json_load = json.loads(data.replace('}]}"},', '}]}"}'))
-            if df.shape[0] == 0:
-                df = pd.json_normalize(json_load['ads'])
-            else:
-                new_df = pd.json_normalize(json_load['ads'])
-                df = df.append(new_df)
-            offset_var = offset_var + limit_var
+                json_load = json.loads(data)
+                if df.shape[0] == 0:
+                    df = pd.json_normalize(json_load['ads'])
+                else:
+                    new_df = pd.json_normalize(json_load['ads'])
+                    df = df.append(new_df)
+                offset_var = offset_var + limit_var
         cate = cate + 1
     return df
